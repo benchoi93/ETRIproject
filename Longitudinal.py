8271dea1ceeb5f684124e9ce5a34ce1c516ba7e9
Seongjin Choi <benchoi93@gmail.com>
2018-04-10 17:58:35 +0900

    Update Longitudinal.py

--- Longitudinal.py
+++ Longitudinal.py
@@ -3,19 +3,19 @@ import numpy as np
 m = 6
 l = 4
 
-N = np.zeros((l,m))
-maxN = np.ones((l,m)) *20 #veh
-Y = np.zeros((l,m+1))
-maxY = np.ones((l,m+1)) * 1800 #veh/hr
-V  = np.zeros((l,m))
-Vf = np.ones((l,m)) * 50
+N = np.zeros((l,m))             #veh
+maxN = np.ones((l,m)) *20       #veh
+Y = np.zeros((l,m+1))           #veh/1dt
+maxY = np.ones((l,m+1)) * 1800  #veh/hr
+V  = np.zeros((l,m))            #km/hr
+Vf = np.ones((l,m)) * 50        #km/hr
 
-L = np.ones(m) * 100
-w = 15
-dt = 5
+L = np.ones(m) * 100    #m
+w = 15                  #km/hr
+dt = 5                  #sec
 
-Lmin = Vf/3.6 * dt
-yin = np.ones((l))
+Lmin = Vf/3.6 * dt      #m
+yin = np.ones((l))      #veh/1dt
 
 
 Y[:,0]  = yin
