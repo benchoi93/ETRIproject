1747a3800e2c4c5e57160287fa6dc34faee3267d
Seongjin Choi <benchoi93@gmail.com>
2018-04-10 18:02:36 +0900

    Update Longitudinal.py

--- Longitudinal.py
+++ Longitudinal.py
@@ -1,21 +1,21 @@
 import numpy as np
 
-m = 6
-l = 4
+m = 6 # number of cells in current link
+l = 4 # maximum number of lanes in current link
 
-N = np.zeros((l,m))             #veh
-maxN = np.ones((l,m)) *20       #veh
-Y = np.zeros((l,m+1))           #veh/1dt
-maxY = np.ones((l,m+1)) * 1800  #veh/hr
-V  = np.zeros((l,m))            #km/hr
-Vf = np.ones((l,m)) * 50        #km/hr
+N = np.zeros((l,m))             # number of vehicles [veh]
+maxN = np.ones((l,m)) *20       # Max. number of vehicles [veh]
+Y = np.zeros((l,m+1))           # Outflow [veh/1dt]
+maxY = np.ones((l,m+1)) * 1800  # capacity flow [veh/hr]
+V  = np.zeros((l,m))            # Cell average speed [km/hr]
+Vf = np.ones((l,m)) * 50        # Cell free flow speed [km/hr]
 
-L = np.ones(m) * 100    #m
-w = 15                  #km/hr
-dt = 5                  #sec
+L = np.ones(m) * 100    # Length of the Cells [m]
+w = 15                  # wave speed [km/hr]
+dt = 5                  # simulation time step [sec]
 
-Lmin = Vf/3.6 * dt      #m
-yin = np.ones((l))      #veh/1dt
+Lmin = Vf/3.6 * dt      # minimum cell length (CTM assumption) [m]
+yin = np.ones((l))      # Link inflow [veh/1dt]
 
 
 Y[:,0]  = yin
