79deed9c2878123346bfc25050410c2e7eba2544
Yeeun <yeeun0802@kaist.ac.kr>
2018-05-01 17:34:10 +0900

    Update TrafficSimulation.cu

--- TrafficSimulation.cu
+++ TrafficSimulation.cu
@@ -1,6 +1,11 @@
-#include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/time.h>
+#include <math.h>
+#include "TrafficSim.h"
+#include <time.h>
 #test branch
 
 
