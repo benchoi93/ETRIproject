be3a2c6f882f367ab93cd8726416835614265902
Yeeun <yeeun0802@kaist.ac.kr>
2018-09-20 13:01:38 +0900

    Update TrafficSim.h

--- TrafficSim.h
+++ TrafficSim.h
@@ -4,38 +4,40 @@
 #define NUM_LANE    		4	/// Number of lane is fixed to 4
 #define NUM_SECTION 		4	/// Number of section is fixed to 4
 #define MAX_VEC     		20	/// Number of vehicle in one cell is limited to 20
-#define MAX_LOOP    		100	/// Number of simulation loop(simulation time step) is limited to 100
-#define MAX_LEG			5	/// Number of intersection leg is limited to 3
+#define MAX_SOURCE_VEC     	100	/// Number of vehicle in one source cell is limited to 100
+#define MAX_SINK_VEC     	100	/// Number of vehicle in one sink cell is limited to 100
+#define MAX_PATH			120	/// Number of path of a vehicle is limited to 120
+#define MAX_LOOP    		300	/// Number of simulation loop is limited to 100
+#define MAX_LEG				3	/// Number of intersection leg is limited to 3
+#define MAX_CYCLE_DURATION  100 /// Max duration of traffic signal cycle is fixed as 100 simulation time step
 #define SECONDS_PER_STEP   	5	/// Time interval of one simulation loop is fixed as 5 seconds
-#define UPDATE_INTERVAL 	60 	/// Time interval between vehicle update is fixed as 60 simulation time step
-#define MAX_CYCLE_DURATION	100	/// Max duration of traffic signal cycle is fixed as 100 simulation time step
+#define UPDATE_INTERVAL 	60 	/// Time interval between vehicle update is fixed as 300 seconds
 
 #define MIN(a,b) (((a)<(b)) ? (a):(b))
 #define MAX(a,b) (((a)>(b)) ? (a):(b))
 
 
 typedef struct {
-	int vehID;		/// ID of vehicle: 1~45
+
+	int vehID;		/// ID of vehicle
 	int vehType;	/// Type of vehicle
 
-	int path[120];	/// Array of Link IDs in the order in which vechicle passes
-	int lenPath;	/// Length of path
+	int path[MAX_PATH];	/// Array of Link IDs in the order in which vechicle passes
+	int lenPath;		/// Length of path
 
-	int minTargetLane[120];	/// Minimum Target Lane
-	int maxTargetLane[120];	/// Maximum Target Lane
-
-	int departTime; 	/// depart time
+	int minTargetLane[MAX_PATH];	/// Minimum Target Lane
+	int maxTargetLane[MAX_PATH];	/// Maximum Target Lane
 
 	int initLink;		/// initial Link ID
-
+	int departTime; 	/// depart time from source cell
 	
 } vehicle;
 
 
 typedef struct {
-	int linkID;		/// ID of link: 1~18
-					/// 11~14: source, 15~18: sink
 
+	int linkID;		/// ID of link
+	
 	double ffSpeed;								/// Free flow speed: 16(m/s)
 	double lenSection[NUM_SECTION+2];			/// Length of section: 100(m)
 	int maxNumVeh[NUM_SECTION+2][NUM_LANE];   	/// Maximum possible number of vehicles in one cell
@@ -65,32 +67,53 @@ typedef struct {
 
 
 typedef struct {
+
+	int sourceID;	/// ID of source cell
+
+	int numVeh; 	 	  			/// Number of vehicles
+	int vehIDArr[MAX_SOURCE_VEC];	/// ID of vehicles
+	
+} source_cell;
+
+
+typedef struct {
+
+	int sinkID;		/// ID of sink cell
+
+	int numVeh;  	  				/// Number of vehicles
+	int vehIDArr[MAX_SINK_VEC];		/// ID of vehicles
+	int arrivalTime[MAX_SINK_VEC];	/// arrival time to sink cell
+
+} sink_cell;
+
+
+typedef struct {
+
 	int ccID;	/// ID of connection cell
 
-	int prevLinkID;			 		/// ID of previous link, equal to ID of connection cell
+	int prevLinkID;			 			/// ID of previous link, equal to ID of connection cell
 	int nextLinkID[NUM_LANE][MAX_LEG];	/// ID of next link
-	int nextLane[NUM_LANE][MAX_LEG];		/// Next lane of next link
-	
-	int cycleDuration;			/// duration of one traffic cycle
-	
-	int trafficSignal[NUM_LANE][MAX_CYCLE_DURATION];	/// Traffic signal information
-						/// Red light: 0, Green light: 1
+	int nextLane[NUM_LANE][MAX_LEG];	/// Next lane of next link
 
-	int numVehArr[NUM_LANE][MAX_LEG];		/// 2D array to store number of vehicles
-
+	int numVehArr[NUM_LANE][MAX_LEG];	/// 2D array to store number of vehicles
 	double numCFArr[NUM_LANE][MAX_LEG];	/// 2D array to store number of CF
 
-	int currLinkOrderArr[NUM_LANE][MAX_VEC]; /// 2D array to store current link order
-	int nextLinkIDArr[NUM_LANE][MAX_VEC];	/// 2D array to store ID of next link of vehicles
+	int currLinkOrderArr[NUM_LANE][MAX_VEC];	/// 2D array to store current link order
+	int nextLinkIDArr[NUM_LANE][MAX_VEC];		/// 2D array to store ID of next link of vehicles
 	int vehIDArr[NUM_LANE][MAX_VEC];			/// 2D array to store ID of vehicles
 	
+	int trafficSignal[NUM_LANE][MAX_CYCLE_DURATION];	/// Traffic signal information
+														/// Red light: 0, Green light: 1
+	int cycleDuration;            						/// duration of one traffic cycle (simulation step)
+
 } connection_cell;
 
-
+/// Setup functions 
 void Setup_Veh(vehicle*, int);
-void Setup_Link(vehicle*, int, link*, int);
+void Setup_Link(vehicle*, int, link*, source_cell*, sink_cell*, int);
 void Setup_ConnectionCell(connection_cell*, int);
 
+/// LC functions
 void Evaluate_MLC(link*);
 void Evaluate_OLC(link *l);
 	int Evaluate_Prob(double);
@@ -98,24 +121,31 @@ void Evaluate_OLC(link *l);
 void LCSim(link*);
 	void MoveLC(int*, int, int*, int, int);
 
+/// CF functions
 void Evaluate_CF(link*);
 void CFsim(link*);
 	void MoveCF(int*, int, int*, int, int);
 
+/// CC functions
 void Update_tempArr(link*);
 	int Find_Index(int*, int, int);
 void Relay_numVeh(link*, link*, int, connection_cell*, int, int);
-void Update_numCF(link*, link*, int, connection_cell*, int, int, int);
+void Relay_numCF(link*, link*, int, connection_cell*, int, int, int);
 void Evaluate_Eff_numCF(link*);
-void Update_vehCF(link*);
 void Update_nextLink(vehicle*, link*, link*, int, connection_cell*, int);
 
+/// Reset functions
 void Reset_ConnectionCell(connection_cell*);
 void Reset_Link(link*);
+void Reset_Sink(sink_cell*);
 
-void SimulationStep(vehicle*, int, link l[], int, connection_cell cc[], int, int);
+/// Source functions
+void Update_Source(vehicle*, int, source_cell* sc, int);
+void Enter_Link(link* l, source_cell* sc, int);
+
+void SimulationStep(vehicle*, int, link l[], int, source_cell sc[], sink_cell sk[], connection_cell cc[], int, int);
 
 double get_time_ms();
-void PrintAll(link l[], int);
+void PrintOutput(link l[], int);
 
 #endif /* TRAFFICSIM_H_ */
